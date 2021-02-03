ba0c4b2bab3fbf76b8fc2b5cd8fbc999f44cf4e7
Wubeshet Yimam <wubeane@gmail.com>
2021-02-03 22:12:25 +0300

    Task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the usual entry point
+ * Return: 0 if execution succeed
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
