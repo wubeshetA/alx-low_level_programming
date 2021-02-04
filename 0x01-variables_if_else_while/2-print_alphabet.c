cf48d72604753c80ac6856c7e921e61756100cfa
Wubeshet Yimam <wubeane@gmail.com>
2021-02-04 19:20:32 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - the usual entry point
+  * Return: 0 is execution succeed
+  */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
