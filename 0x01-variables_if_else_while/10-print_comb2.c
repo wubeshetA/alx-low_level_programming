3a10d711fb2c31ae8ef437db01f665c7331d1632
Wubeshet Yimam <wubeane@gmail.com>
2021-02-04 22:27:57 +0300

    Create 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - the usual entry point
+  * Return: 0 if program execution succeed
+  */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+	while (i <= 57)
+	{
+		while (j <= 57)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 57 || j != 57)
+				putchar(',');
+			putchar(32);
+			j++;
+		}
+		i++;
+		j = 48;
+	}
+	return (0);
+}
