a361d35748662c8aa1cde0f6a88001c5f139e070
Wubeshet Yimam <wubeane@gmail.com>
2021-02-05 16:42:41 +0300

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - the usual entry point
+ * Return: 0 if execution succeed
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 48;
+	j = 49;
+	k = 50;
+	while (i <= 57)
+	{
+		while (j <= 57)
+		{
+			while (k <= 57 && i != j && j != k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55)
+				{
+					putchar(',');
+					putchar(32);
+				}
+				k++;
+			}
+			j++;
+			k = j + 1;
+		}
+		i++;
+		j = i + 1;
+		k = j + 1;
+	}
+	putchar('\n');
+	return (0);	
+}
