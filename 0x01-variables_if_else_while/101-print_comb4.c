9facb776cd4a1ab9c3a9f8f101134382be123080
Wubeshet Yimam <wubeane@gmail.com>
2021-02-05 20:25:30 +0300

    update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 				putchar(j);
 				putchar(k);
 				if (i != 55 || j != 56 || k != 57)
-				{
+			{
 					putchar(',');
 					putchar(32);
 				}
@@ -35,5 +35,5 @@ int main(void)
 		k = j + 1;
 	}
 	putchar('\n');
-	return (0);	
+	return (0);
 }
