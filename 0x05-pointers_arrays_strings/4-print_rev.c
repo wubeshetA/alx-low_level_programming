9d432af1cf584119205578e89b9a8673289816f2
Wubeshet Yimam <wubeane@gmail.com>
2021-02-17 18:33:00 +0300

    update 4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <string.h>
 
 /**
   * print_rev - print a string in reverse.
@@ -7,11 +6,13 @@
   */
 void print_rev(char *s)
 {
-	int i = strlen(s);
+	int i = 0;
 
+	while (s[i] != '\0')
+		i++;
 	while (i >= 0)
 	{
-		_putchar(s[i]);
+		_putchar(*(s + i));
 		i--;
 	}
 	_putchar('\n');
